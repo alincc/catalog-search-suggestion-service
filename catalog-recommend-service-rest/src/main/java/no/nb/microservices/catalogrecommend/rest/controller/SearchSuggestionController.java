53c73f4291458155e786d84c0a15359f683bad1b
ronnymikalsen <ronny.mikalsen@nb.no>
2015-10-27 12:33:02 +0100

    refactor

--- catalog-recommend-service-rest/src/main/java/no/nb/microservices/catalogrecommend/rest/controller/SearchSuggestionController.java
+++ catalog-recommend-service-rest/src/main/java/no/nb/microservices/catalogrecommend/rest/controller/SearchSuggestionController.java
@@ -1,27 +1,29 @@
 package no.nb.microservices.catalogrecommend.rest.controller;
 
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiResponse;
-import com.wordnik.swagger.annotations.ApiResponses;
-import no.nb.microservices.catalogrecommend.core.searchsuggestions.model.Suggestion;
-import no.nb.microservices.catalogrecommend.core.searchsuggestions.service.ISearchSuggestionService;
-import no.nb.microservices.catalogrecommend.rest.model.SearchSuggestionResource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
+
+import no.nb.microservices.catalogrecommend.core.searchsuggestions.model.Suggestion;
+import no.nb.microservices.catalogrecommend.core.searchsuggestions.service.ISearchSuggestionService;
+import no.nb.microservices.catalogrecommend.rest.model.SearchSuggestionResource;
+
 @RestController
 @Api(value = "/catalog/", description = "Home api")
-@RequestMapping(value = "/catalog/")
+@RequestMapping(value = "/catalog/recommend/")
 public class SearchSuggestionController {
 
     private final ISearchSuggestionService searchSuggestionService;
@@ -33,9 +35,10 @@ public class SearchSuggestionController {
 
     @ApiOperation(value = "Hello World", notes = "Hello World notes", response = String.class)
     @ApiResponses(value = { @ApiResponse(code = 200, message = "Successful response") })
-    @RequestMapping(value = "{currentSite}/suggestions", method = RequestMethod.GET)
-    public ResponseEntity<List<SearchSuggestionResource>> getSuggestions(@PathVariable(value = "currentSite") String currentSite) {
-        List<Suggestion> suggestions = searchSuggestionService.getSuggestions(currentSite);
+    @RequestMapping(value = "/suggestions", method = RequestMethod.GET)
+    public ResponseEntity<List<SearchSuggestionResource>> getSuggestions(
+            @RequestParam(value = "site", defaultValue="nbdigital") String site) {
+        List<Suggestion> suggestions = searchSuggestionService.getSuggestions(site);
 
         if(suggestions != null) {
             List<SearchSuggestionResource> body = suggestions.stream()
